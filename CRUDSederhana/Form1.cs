5c951f1ad516c3241fa2c561b6a74f7eae4fa288
Damar2121 <damar.vivoy51@gmail.com>
2025-03-21 16:41:03 +0700

    Event saat form pertama kali dimuat

--- CRUDSederhana/Form1.cs
+++ CRUDSederhana/Form1.cs
@@ -13,10 +13,14 @@ namespace CRUDSederhana
 {
     public partial class Form1 : Form
     {
-        // Ganti "SERVER" sesuai dengan SQL Server Anda
         private string connectionString = "Data Source= LAPTOP-LM2B4178\\DAMARSADEWAPUTRA;" + "Initial Catalog=OrganisasiMahasiswa;Integrated Security=True";
 
         public Form1()
         {
             InitializeComponent();
         }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
